de4a68adad3333c0f7c0d59a1f8a92d3b18e51a6
iqbalyarkhan <iqbal.khan@utdallas.edu>
2018-08-19 18:13:09 -0500

    Add sample scraper package

--- main/main.go
+++ main/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"ner"
-	"os"
+	"scraper"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text to tokenize: ")
-	text, _ := reader.ReadString('\n')
-	//fmt.Println(text)
-	ner.TestNer(text)
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter text to tokenize: ")
+	// text, _ := reader.ReadString('\n')
+	// //fmt.Println(text)
+	// ner.TestNer(text)
+	scraper.SampleScrape()
 }
